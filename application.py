5644a77720375d3d02813e9bae292ab0e73ea3c5
andersjh <andersjh120@gmail.com>
2021-06-15 18:21:48 -0500

    redo for aws

--- application.py
+++ application.py
@@ -18,19 +18,19 @@ else:
 #################################################
 # Flask Setup
 #################################################
-app = Flask(__name__)
+application = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
 
-@app.route("/")
+@application.route("/")
 def welcome():
     users = data.get_subject_ids()
     return render_template("index.html", user_ids=users)
 
-@app.route("/api/v1.0")
+@application.route("/api/v1.0")
 def show_apis():
     """List all available api routes."""
     return (
@@ -42,27 +42,27 @@ def show_apis():
         f'<a href="/"><h4>Back</h4></a><br/>' 
     )    
 
-@app.route("/api/v1.0/ids")
+@application.route("/api/v1.0/ids")
 def get_all_ids():
     return jsonify(data.get_subject_ids())
 
-@app.route("/api/v1.0/info")
+@application.route("/api/v1.0/info")
 def get_all_results():
     return jsonify(data.get_data_for_all())    
 
-@app.route("/api/v1.0/info/<subject_id>")
+@application.route("/api/v1.0/info/<subject_id>")
 def get_one_user_results(subject_id):
     return jsonify(data.get_data_by_user(subject_id))    
 
-@app.route("/api/v1.0/subjects")
+@application.route("/api/v1.0/subjects")
 def get_all_subjects():
     return jsonify(data.get_subjects())
 
-@app.route("/api/v1.0/subjects/<subject_id>")
+@application.route("/api/v1.0/subjects/<subject_id>")
 def get_one_subject(subject_id):
     return jsonify(data.get_subjects(subject_id))
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
